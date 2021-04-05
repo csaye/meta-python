8be71e296e8729b804596d814a046996923b6f39
Cooper Saye <coopersaye@gmail.com>
2021-04-05 12:23:26 -0600

    Implement variable assignment.

--- Interpreter.py
+++ Interpreter.py
@@ -1,3 +1,16 @@
+# globals
+vardict = {}
+
+# whether string is int
+def is_int(string):
+    try: int(string); return True
+    except: return False
+
+# whether string is float
+def is_float(string):
+    try: float(string); return True
+    except: return False
+
 # processes given line
 def process_line(line):
     # print
@@ -7,6 +20,20 @@ def process_line(line):
         content = content[1:(len(content) - 1)].strip()
         # print content without quotations
         print(content[1:(len(content) - 1)])
+    # variable
+    if '=' in line:
+        equal_index = line.find('=')
+        # get variable and value
+        var = line[:equal_index].strip()
+        val = line[(equal_index + 1):].strip()
+        # get result
+        if is_int(val): res = int(val)
+        elif is_float(val): res = float(val)
+        elif val in vardict.keys(): res = vardict[val]
+        else: res = val[1:(len(val) - 1)]
+        # set result
+        vardict[var] = res
+        print(vardict)
 
 # read input
 fin = open('./Input.py', 'r')

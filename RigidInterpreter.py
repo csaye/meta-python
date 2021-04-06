3e8acd8f40e00bd0e021eb8399ccb5b352406c1b
Cooper Saye <coopersaye@gmail.com>
2021-04-06 10:42:52 -0600

    Implement rigid value parsing.

--- RigidInterpreter.py
+++ RigidInterpreter.py
@@ -1,3 +1,37 @@
+# globals
+var_dict = {}
+
+# returns whether string is int
+def is_int(string):
+    try: int(string); return True
+    except: return False
+
+# returns whether string is float
+def is_float(string):
+    try: float(string); return True
+    except: return False
+
+# parses given expression
+def parse_expression(terms):
+    return terms
+
+# parses given term
+def parse_term(term):
+    if is_int(term): return int(term)
+    elif is_float(term): return float(term)
+    elif term in var_dict.keys(): return var_dict[term]
+    else: return term[1:(len(term) - 1)]
+
+# parses given value
+def parse_value(value):
+    terms = value.split()
+    # if no terms, return none
+    if len(terms) == 0: return None
+    # if one term, parse term
+    elif len(terms) == 1: return parse_term(terms[0])
+    # if multiple terms, parse expression
+    else: return parse_expression(terms)
+
 # parses given line
 def parse_line(raw_line):
     # strip line
